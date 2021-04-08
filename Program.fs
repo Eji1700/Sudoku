759e08b75de1fdcb53c7625af17acaf96187df02
dungarDot <regulus87@yahoo.com>
2021-04-07 20:57:39 -0700

    Hard coding cosole init for testing

--- Program.fs
+++ Program.fs
@@ -3,10 +3,10 @@ open GameLogic
 
 [<EntryPoint>]
 let main argv =
-    
-    match argv with
-    | [||] -> ()
-    | _ -> 
-        UI.ConsoleOutput.Init()
+    UI.ConsoleOutput.Init() //remove me
+    // match argv with
+    // | [||] -> ()
+    // | _ -> 
+    //     UI.ConsoleOutput.Init()
     GameLoop Initial.Game
     0

cc3fc0a33e9bc3e8d70498f2e1980012baf382b7
Eji1700 <regulus87@yahoo.com>
2021-06-05 00:20:05 -0700

    quick and dirty file input so boards can be generated without editiing code

--- GameSettings.fs
+++ GameSettings.fs
@@ -1,8 +1,9 @@
 namespace GameSettings
 open GameParts
+open System.IO
 
 module Initial =
-  let private board  =
+  let private defaultBoard  =
     array2D [|
       [|3;0;0;5;7;0;2;4;0|]
       [|5;7;2;0;4;0;9;0;0|]
@@ -16,7 +17,17 @@ module Initial =
     |] 
     |> Board.Create
 
+  let private fileBoard =
+    let sourceDir = __SOURCE_DIRECTORY__
+    let testFile = Directory.GetFiles(sourceDir,"board.txt")
+    let data = File.ReadLines(testFile.[0])
+    data
+    |> Seq.map (fun s -> s.Split(","))
+    |> Seq.map( fun arr -> arr |> Array.map (fun s -> s |> int))
+    |> fun seq -> array2D seq 
+    |> Board.Create
+
   let Game = 
-    { Board = board 
+    { Board = fileBoard 
       State = StartGame
       ActiveCell = 0,1 }
--- Program.fs
+++ Program.fs
@@ -3,14 +3,8 @@ open GameLogic
 
 [<EntryPoint>]
 let main argv =
-    UI.ConsoleOutput.Init() //remove me and put in argu console parsing?
-    // match argv with
-    // | [||] -> ()
-    // | _ -> 
-    //     UI.ConsoleOutput.Init()
-    
+    UI.ConsoleOutput.Init() //remove me and put in argu console parsing?    
     GameLoop Initial.Game
-    // |> printfn "%A"
     0
 
     // todo
@@ -19,4 +13,4 @@ let main argv =
     // Highlight all errors on solution check, clear with e?
     // Dear god the Console UI
     // At least need a "clear line" function to help blank things out
-    // Argu integration and eventually real frontend.
+    // Argu integration and eventually real frontend.s

043cc787160b57dee95a4eec5f3796449fbbf4b9
dungarDot <regulus87@yahoo.com>
2021-07-17 13:19:04 -0700

    removed dupe board, made defualt a test board.

--- GameSettings.fs
+++ GameSettings.fs
@@ -3,24 +3,9 @@ open GameParts
 open System.IO
 
 module Initial =
-    let defaultBoard  =
+    let testBoard  =
         [|
         [|Some 3;None; None;Some 5;Some 7;None;Some 2;Some 4;None|]
-        [|Some 5;Some 7; Some 2;None;Some 4;None;Some 9;None;None|]
-        [|None;None; Some 4;None;Some 2;Some 6;Some 5;None;Some 3|]
-        [|Some 7;None; Some 3;None;None;Some 2;None;Some 8;Some 1|]
-        [|Some 4;Some 2; None;Some 1;None;Some 7;None;None;Some 9|]
-        [|None;None; Some 6;None;Some 3;Some 4;Some 7;None;Some 5|]
-        [|Some 6;Some 3; None;Some 2;None;Some 5;None;Some 9;None|]
-        [|None;Some 4; None;Some 7;Some 6;Some 3;None;Some 5;None|]
-        [|None;Some 8; Some 5;None;None;None;Some 6;Some 3;Some 7|]
-        |] 
-        |> array2D
-        |> Board.Create
-
-    let dupeBoard  =
-        [|
-        [|Some 3;Some 3; None;Some 5;Some 7;None;Some 2;Some 4;None|]
         [|Some 5;Some 7; Some 2;None;Some 4;None;Some 9;None;None|]
         [|None;None; Some 4;None;Some 2;Some 6;Some 5;None;Some 3|]
         [|Some 7;None; Some 3;None;None;Some 2;None;Some 8;Some 1|]

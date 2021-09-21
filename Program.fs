95e155833ff145b259d3b33d3eaf5c4f3cbef603
dungarDot <regulus87@yahoo.com>
2021-09-20 23:45:46 -0700

    Adjusting TODOs

--- Program.fs
+++ Program.fs
@@ -10,10 +10,7 @@ let main argv =
 
     // todo
     // Redo types to prevent bad states.
-    // Marked/Wrong options
     // Mark cell for testing with m
-    // Highlight all errors on solution check, clear with e?
     // Dear god the Console UI
-    // At least need a "clear line" function to help blank things out
     // Argu integration and eventually real frontend.
     // Dynamic active starting cell so as not to screw up puzzles
